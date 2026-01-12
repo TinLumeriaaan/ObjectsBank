d74f0fb832686a2e9324892f6acd3b04e88d42f0
Chrissydntes <christinedantes1604@gmail.com>
2026-01-12 22:12:38 +0800

    Header and footer code

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+</section>
+    <footer style="text-align: center; padding: 20px;">
+      <p>&copy; 2026 Dantes, Christine. All rights reserved.</p>
+    </footer>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <link href="css/styles.css?v=5" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <a href="bank.php"><img src="img/banklogo.png" alt="Bank Logo" /></a>
+    </header>
+    <section>
